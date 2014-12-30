0a245ddaaaef99e0d012c36f0218a00f57a7b4ed
Lee Essen <lee.essen@nowonline.co.uk>
2014-12-30 14:21:42 +0000

    added 9p support

--- simulate.sh
+++ simulate.sh
@@ -15,6 +15,7 @@ ROOTFS=${BUILDROOT}/output/images/rootfs.ext2
 LINUX_ARGS="root=/dev/hda console=ttyS0"
 BASE_NET="-netdev user,id=main,host=10.1.0.1,net=10.1.0.0/24,dhcpstart=10.1.0.16 -device pcnet,netdev=main"
 EXTRA_NET="-device pcnet"
+FS="-fsdev local,id=joe,security_model=none,path=/tmp -device virtio-9p-pci,fsdev=joe,mount_tag=/dev/joetag"
 
 
 ${QEMU} ${QEMU_OPTIONS} \
@@ -22,6 +23,7 @@ ${QEMU} ${QEMU_OPTIONS} \
 	-drive file=${ROOTFS} \
 	-append "${LINUX_ARGS}" \
 	${BASE_NET} \
-	${EXTRA_NET}
+	${EXTRA_NET} \
+	${FS}
 
 #qemu-system-mips -nographic -kernel output/images/vmlinux -drive file=output/images/rootfs.ext2 -append "root=/dev/hda console=ttyS0" -netdev user,id=fred -device pcnet,netdev=fred
