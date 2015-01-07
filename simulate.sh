b825e4e9299dd55ec549203fa30fae538df8cf40
Lee Essen <lee.essen@nowonline.co.uk>
2015-01-07 07:05:25 +0000

    support for tap device (ppp testing)

--- simulate.sh
+++ simulate.sh
@@ -17,6 +17,7 @@ BASE_NET="-netdev user,id=main,host=10.1.0.1,net=10.1.0.0/24,dhcpstart=10.1.0.16
 EXTRA_NET="-device pcnet"
 NO_CLOCK="-rtc base=1970-01-01T12:00:00,clock=vm"
 FS="-fsdev local,id=netcamel,security_model=none,path=./lua -device virtio-9p-pci,fsdev=netcamel,mount_tag=netcamel"
+TAP_NET="-netdev tap,ifname=tap0,script=no,downscript=no,id=tap -device pcnet,netdev=tap"
 
 
 ${QEMU} ${QEMU_OPTIONS} \
@@ -24,8 +25,8 @@ ${QEMU} ${QEMU_OPTIONS} \
 	-drive file=${ROOTFS} \
 	-append "${LINUX_ARGS}" \
 	${BASE_NET} \
-	${EXTRA_NET} \
 	${FS} \
-	${NO_CLOCK}
+	${NO_CLOCK} \
+	${TAP_NET}
 
 #qemu-system-mips -nographic -kernel output/images/vmlinux -drive file=output/images/rootfs.ext2 -append "root=/dev/hda console=ttyS0" -netdev user,id=fred -device pcnet,netdev=fred
