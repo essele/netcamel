02dd65890fae2192a10a899b25fa36042a162bc5
Lee Essen <lee.essen@nowonline.co.uk>
2015-01-17 06:27:39 +0000

    support shared network

--- simulate.sh
+++ simulate.sh
@@ -13,8 +13,9 @@ QEMU_OPTIONS=-nographic
 KERNEL=${BUILDROOT}/output/images/vmlinux
 ROOTFS=${BUILDROOT}/output/images/rootfs.ext2
 LINUX_ARGS="root=/dev/hda console=ttyS0"
-BASE_NET="-netdev user,id=main,host=10.1.0.1,net=10.1.0.0/24,dhcpstart=10.1.0.16,hostfwd=tcp::8022-10.1.0.16:22 -device pcnet,netdev=main"
-EXTRA_NET="-device pcnet"
+#BASE_NET="-netdev user,id=main,host=10.1.0.1,net=10.1.0.0/24,dhcpstart=10.1.0.16,hostfwd=tcp::8022-10.1.0.16:22 -device pcnet,netdev=main"
+#EXTRA_NET="-device pcnet"
+BASE_NET="-netdev socket,id=lan,listen=localhost:10234 -device pcnet,netdev=lan"
 NO_CLOCK="-rtc base=1970-01-01T12:00:00,clock=vm"
 FS="-fsdev local,id=netcamel,security_model=none,path=./lua -device virtio-9p-pci,fsdev=netcamel,mount_tag=netcamel"
 TAP_NET="-netdev tap,ifname=tap0,script=no,downscript=no,id=tap -device pcnet,netdev=tap"
