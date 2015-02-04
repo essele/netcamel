59b515fd0d17f2d2a0abbbce05e64ac613414392
Lee Essen <lee.essen@nowonline.co.uk>
2015-02-04 10:27:09 +0000

    new readline lib

---
+++ lua/lib/readline2.lua
@@ -0,0 +1,253 @@
+#!/usr/bin/luajit
+--------------------------------------------------------------------------------
+--  This file is part of NetCamel
+--  Copyright (C) 2014,15 Lee Essen <lee.essen@nowonline.co.uk>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------------------------------------
+
+--
+-- Return a token for each call, once we get to the end the flag the end
+-- state (calling again is undefined)
+--
+local function get_token(state, sep)
+	-- update token number and ensure token table setup
+	state.n = (state.n and state.n+1) or 1
+	state.tokens = state.tokens or {}
+	sep = sep or "%z"
+
+	-- if previous was last then return nil and clear our old tokesn
+	if state.final then
+		while state.tokens[state.n] do table.remove(state.tokens, state.n) end
+		return nil 
+	end
+
+	-- find the token (or empty)
+	local start, finish = state.value:find("[^"..sep.."]+", state.pos)
+	if not start then start = #state.value + 1 finish = start - 1 end
+
+	-- work out if we are the last and setup pos for next time
+	state.final = start == #state.value or finish == #state.value or nil
+	state.pos = finish + 1
+
+	-- see if we have an existing token, if not create a new one
+	local token = state.tokens[state.n] or {}
+	local value = state.value:sub(start, finish)
+
+	-- set flags for change/nochange
+	token.samevalue = value == token.value
+	token.nochange = value == token.value and token.start == start and token.finish == finish
+
+	-- if we have changed value then we need to remove all futures since they need recheck
+	if not token.samevalue then
+		while state.tokens[state.n+1] do table.remove(state.tokens, state.n+1) end
+	end
+
+	token.start = start
+	token.finish = finish
+	token.value = value
+	token.final = state.final
+
+	-- make sure our state is accurate
+	state.tokens[state.n] = token
+	return token
+end
+
+--
+-- Before we start getting tokens we need to ensure our state is clean
+--
+local function reset_state(state)
+	state.pos = nil
+	state.n = nil
+	state.final = nil
+end
+
+--
+-- String manipulation (should be in utils)
+--
+function string_insert(s, i, pos) return s:sub(1,pos-1) .. i .. s:sub(pos) end
+function string_remove(s, pos, count) return s:sub(1,pos-1) .. s:sub(pos+count) end
+
+--
+-- Go backwards to the nearest word start
+--
+local function bword(line, pos)
+    if pos <= 1 then return 0 end
+
+    pos = pos - 1
+    while pos >= 1 do
+        if line:sub(pos+1, pos+1):match("%w") and not line:sub(pos, pos):match("%w") then return pos end
+        pos = pos - 1
+    end
+    return 0
+end
+--
+-- Go to the next non-alpha after an alpha
+--
+local function fword(line, pos)
+	if pos >= line:len() then return pos end
+	pos = pos + 1
+	while pos < line:len() do
+		if not line:sub(pos+1, pos+1):match("%w") and line:sub(pos, pos):match("%w") then return pos end
+		pos = pos + 1
+	end
+	return line:len()
+end
+
+--
+-- Efficiently output the line working on things that have specifically changed
+-- only. We treat all adjacent separators as a single coloured entity and it will
+-- be output in its entirely if needed (hence the need for p to be outside the func)
+--
+local function display_line(prompt, state, force)
+	local p = 1
+	local value = state.value
+
+	local function token_output(tokens, offset, force)
+		-- process the tokens
+		for _,token in ipairs(tokens) do
+			if not token.nochange or force then
+				if token.tokens then
+					token_output(token.tokens, offset+token.start-1, force)
+				else
+					-- leading separators
+					lib.term.set_color(token.color or "red")
+					if offset+token.start > p then
+						lib.term.move_to_pos(prompt.len+p-1)
+						lib.term.output(value:sub(p, offset+token.start-1))
+					end
+					-- main token
+					lib.term.move_to_pos(prompt.len+offset+token.start-1)
+					lib.term.output(value:sub(offset+token.start, offset+token.finish))
+				end
+				lib.term.set_color("default")
+			end
+			p = offset+token.finish+1
+		end
+		lib.term.set_color("default")
+	end
+
+	if prompt.show or force then
+		lib.term.move_to_pos(0)
+		lib.term.set_color("bright blue")
+		lib.term.output(prompt.value)
+	end
+
+	token_output(state.tokens, 0, force)
+	lib.term.move_to_pos(prompt.len + #state.value)
+	lib.term.clear_to_eol()
+end
+
+--
+-- Read a line in and handle history, as well as syntax and completion
+-- callbacks
+--
+local function read_command(prompt, history, syntax_cb, complete_cb)
+	lib.term.push("raw")				-- raw term
+	table.insert(history, "")			-- space for our line
+	local line, hline = "", ""			-- running buffer, history line
+	local pos, oldpos = 0, 0			-- cursor pos
+	local state = { value = line }		-- starting point
+	local chg = true					-- show prompt first time
+	local hpos = #history				-- at end of history
+
+	prompt.show = true					-- show prompt first time
+
+	while true do
+		--
+		-- Process (syntax_cb) and output the line if needed
+		--
+		if chg then
+			state.value = line
+			reset_state(state)
+			syntax_cb(state)
+			display_line(prompt, state)
+			lib.term.move_to_pos(prompt.len + pos)
+		elseif pos ~= oldpos then
+			lib.term.move_to_pos(prompt.len + pos)
+		end
+		io.flush()
+
+		--
+		-- Reset state variables for the next user input
+		--
+		oldpos = pos
+		chg = false
+
+		--
+		-- Process input
+		--
+		local x = lib.term.read()
+		if x == "q" then break end
+
+		if x == "BACKSPACE" then if pos > 0 then line = string_remove(line, pos, 1) chg = true pos = pos - 1 end
+		elseif x == "DELETE" then if pos < #line+1 then line = string_remove(line, pos+1, 1) chg = true end
+		elseif x == "LEFT" then if pos > 0 then pos = pos - 1 end
+		elseif x == "RIGHT" then if pos < #line then pos = pos + 1 end
+		elseif x == "UP" or x == "DOWN" then
+			if (x == "UP" and hpos > 1) or (x == "DOWN" and hpos < #history) then 
+				history[hpos] = line
+				hpos = hpos + ((x == "UP" and -1) or 1)
+				line = history[hpos] 
+				chg = true 
+				pos = #line 
+			end
+		elseif x == "GO_EOL" then pos = #line
+		elseif x == "GO_BOL" then pos = 0
+		elseif x == "GO_BWORD" then pos = bword(line, pos)
+		elseif x == "GO_FWORD" then pos = fword(line, pos)
+		elseif x == "INT" then 
+			print("^C") 
+			lib.term.reset_pos() 
+			line, pos, chg, prompt.show = "", 0, true, true
+		elseif x == "TAB" then
+			complete_cb(state.tokens, line, pos)
+		else
+			if #x > 1 then x = "?" end
+			line = string_insert(line, x, pos+1) chg = true pos = pos + 1
+		end
+	end
+	lib.term.pop()
+	table.remove(history)
+end
+
+--
+-- Given the tokens and a position work out which token we are in so
+-- that we can run the completer effectively.
+--
+function which_token(tokens, pos)
+	for i,token in ipairs(tokens) do
+		print("i="..i.." pos="..pos)
+		if pos <= token.finish then return i end
+	end
+	return nil
+end
+--
+-- If we are subtoken'd then given the tokens and main token index
+-- and the cursor pos then work out which subtoken.
+--
+function which_subtoken(tokens, n, pos)
+	return which_token(tokens[n].tokens, pos+1 - tokens[n].start)
+end
+
+
+
+return {
+	read_command = read_command,
+	get_token = get_token,
+	reset_state = reset_state,
+	which_token = which_token,
+	which_subtoken = which_subtoken,
+}
+	
--- lua/x.lua
+++ lua/x.lua
@@ -18,84 +18,23 @@
 ------------------------------------------------------------------------------
 
 dofile("lib/lib.lua")
-lib = { term = dofile("./term.lua") }
-
--- ==============================================================================
---
--- Requiring this library will cause subsequent modules to be loaded on demand
--- into the lib table.
---
--- ==============================================================================
 
 --
--- Return a token for each call, once we get to the end the flag the end
--- state (calling again is undefined)
+-- TEST CODE ONLY
 --
-local function get_token(state, sep)
-	-- update token number and ensure token table setup
-	state.n = (state.n and state.n+1) or 1
-	state.tokens = state.tokens or {}
-	sep = sep or "%z"
-
-	-- if previous was last then return nil and clear our old tokesn
-	if state.final then
-		while state.tokens[state.n] do table.remove(state.tokens, state.n) end
-		return nil 
-	end
-
-	-- find the token (or empty)
-	local start, finish = state.value:find("[^"..sep.."]+", state.pos)
-	if not start then start = #state.value + 1 finish = start - 1 end
-
-	-- work out if we are the last and setup pos for next time
-	state.final = start == #state.value or finish == #state.value or nil
-	state.pos = finish + 1
-
-	-- see if we have an existing token, if not create a new one
-	local token = state.tokens[state.n] or {}
-	local value = state.value:sub(start, finish)
-
-	-- set flags for change/nochange
-	token.samevalue = value == token.value
-	token.nochange = value == token.value and token.start == start and token.finish == finish
-
-	-- if we have changed value then we need to remove all futures since they need recheck
-	-- if we are the last item then make sure we clear all subsequent old tokens
-	if not token.samevalue then
-		while state.tokens[state.n+1] do table.remove(state.tokens, state.n+1) end
-	end
-
-	token.start = start
-	token.finish = finish
-	token.value = value
-	token.final = state.final
-
-	-- make sure our state is accurate
-	state.tokens[state.n] = token
-
-	return token
-end
-local function reset_state(state)
-	state.pos = nil
-	state.n = nil
-	state.final = nil
-end
-
-
 function process_cfpath(state)
 	-- pull the token, return if unchanged
-	local path = get_token(state, "%s")
+	local path = lib.readline2.get_token(state, "%s")
 
 	if not path or path.samevalue then return end
 
 	-- update accordingly
-	reset_state(path)
+	lib.readline2.reset_state(path)
 
 	while true do
-		local elem = get_token(path, "/")
+		local elem = lib.readline2.get_token(path, "/")
 		if not elem then break end
 
---		print("got elem: "..elem.value)
 
 		if elem.valid and elem.samevalue then
 --			print("no check needed")
@@ -106,14 +45,26 @@ function process_cfpath(state)
 	end	
 end
 
+--
+-- TEST CODE ONLY
+--
+function completeCB(tokens, line, pos)
+	local i = which_token(tokens, pos)
+
+	if i==2 then
+		local j = which_subtoken(tokens, i, pos)
+		print("j="..j)
+	end
+end
 
 
-function process(state) 
-	reset_state(state)
-	local cmd = get_token(state, "%s")
+--
+-- TEST CODE ONLY
+--
+function processCB(state) 
+	local cmd = lib.readline2.get_token(state, "%s")
 
 	if cmd then
-
 		if cmd.value == "set" then
 			cmd.color = "green"
 			process_cfpath(state)
@@ -125,100 +76,15 @@ function process(state)
 		end
 	end
 
-	local rest = get_token(state)
+	local rest = lib.readline2.get_token(state)
 	if rest then
---			print("rest: ["..rest.value.."]")
+			print("rest: ["..rest.value.."]")
 --			print("rsame: " .. tostring(rest.samevalue))
 	end
 end
 
---
--- String manipulation (should be in utils)
---
-function string_insert(s, i, pos)
-	return s:sub(1,pos-1) .. i .. s:sub(pos)
-end
-function string_remove(s, pos, count)
-	return s:sub(1,pos-1) .. s:sub(pos+count)
-end
+local prompt = { value = "prompt > ", len = 9 }
+local history = { "fred", "one two thre", "set /abc/def/ghi" }
 
-
---
--- Efficiently output the line working on things that have specifically changed
--- only
---
-local function display_line(state, force)
-	local function token_output(tokens, value, offset, force)
-		local p = 1
-		-- process the tokens
-		for _,token in ipairs(tokens) do
-			if not token.nochange or force then
-				lib.term.set_color(token.color or "red")
-
-				-- leading separators
-				if token.start > p then
-					lib.term.move_to_pos(offset+p-1)
-					lib.term.output(value:sub(p, token.start-1))
-				end
-				if token.tokens then
-					token_output(token.tokens, token.value, offset + token.start-1, force)
-				else
-					lib.term.move_to_pos(offset+token.start-1)
-					lib.term.output(token.value)
-				end
-				lib.term.set_color("default")
-			end
-			p = token.finish+1
-		end
-		lib.term.set_color("default")
-
-		-- if we just deleted the last char in the second token and the first one is unchanged
-		-- then we won't have output anything, when we clreol we will delete the first token!
-	end
-	token_output(state.tokens, state.value, 0, force)
-	lib.term.move_to_pos(#state.value)
-	lib.term.clear_to_eol()
-end
-
-
-lib.term.push("raw")
-local line = ""						-- running buffer
-local pos = 0						-- cursor pos
-local state = {}
-while true do
-	local oldpos = pos
-	local chg = false
-
-	local x = lib.term.read()
-	if x == "q" then break end
-
-	if x == "BACKSPACE" then
-		if pos > 0 then	line = string_remove(line, pos, 1) chg = true pos = pos - 1 end
-	elseif x == "DELETE" then
-		if pos < #line+1 then line = string_remove(line, pos+1, 1) chg = true end
-	elseif x == "LEFT" then
-		if pos > 0 then pos = pos - 1 end
-	elseif x == "RIGHT" then
-		if pos < #line then pos = pos + 1 end
-	else
-		line = string_insert(line, x, pos+1) chg = true pos = pos + #x
-	end
-
-	if chg then
-		state.value = line
-		process(state)
-
-	--	lib.term.output(line)
-		lib.term.move_to(0, 0)
-		display_line(state)
---		lib.term.move_to_pos(#line)
-
-		lib.term.move_to_pos(pos)
-	elseif pos ~= oldpos then
-		lib.term.move_to_pos(pos)
-	end
-	io.flush()
-end
-lib.term.pop()
-
+lib.readline2.read_command(prompt, history, processCB, completeCB)
 
