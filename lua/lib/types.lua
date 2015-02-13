412315d6efbac0259e814f0f2560e885db6b3bec
Lee Essen <lee.essen@nowonline.co.uk>
2015-02-13 18:53:03 +0000

    added ipv4 types

--- lua/lib/types.lua
+++ lua/lib/types.lua
@@ -45,22 +45,6 @@ local function validate_std(value, mp, kp, tokens, t)
 end
 
 --
--- Simple 2-digit field
---
-local function validate_2digit(value, mp, kp, tokens, t)
-	local err = "require two digits (nn)"
-	local a, b = v:match("^(%d)(%d?)$")
-	if not a then return FAIL, err end
-	if b == "" then return PARTIAL, err end
-	return OK
-end
-
-
-
-
-
-
---
 -- Main type routine: validate a value is correct (or partial)
 --
 local function validate(value, mp, kp, tokens)
@@ -79,15 +63,77 @@ local function options(mp)
 	return o
 end
 
-
-
+-- ------------------------------------------------------------------------------
+-- BOOLEAN
+-- ------------------------------------------------------------------------------
 TYPE["boolean"] = {
-	validator = validate_std,
+	validator = validate_bool,
 	options = { "true", "false" },
 }
-TYPE["2-digit"] = {
-	validator = validate_2digit,
-}
+TYPE["boolean"].validator = function(value, mp, kp, tokens, t)
+	if v == "true" or v == "yes" or v == "1" then return OK, true end
+	if v == "false" or v == "no" or v == "0" then return OK, false end
+	return partial_match(value, {"true", "false", "yes", "no"})
+end
+
+-- ------------------------------------------------------------------------------
+-- 2-digit
+-- ------------------------------------------------------------------------------
+TYPE["2-digit"] = {}
+TYPE["2-digit"].validator = function(value, mp, kp, tokens, t)
+	local err = "require two digits (nn)"
+	local a, b = v:match("^(%d)(%d?)$")
+	if not a then return FAIL, err end
+	if b == "" then return PARTIAL, err end
+	return OK
+end
+
+-- ------------------------------------------------------------------------------
+-- Normal ipv4 address
+-- ------------------------------------------------------------------------------
+TYPE["ipv4"] = {}
+TYPE["ipv4"].validator = function(value, mp, kp, tokens, t)
+	local nc, err = 0, "ipv4 must be standard dotted quad"
+	if not value:match("^[%d%.]+$") then return FAIL, err end
+	while value:len() > 0 do
+		local dig = value:match("^(%d+)")
+		if not dig or tonumber(dig) > 255 then return FAIL, err end
+		nc = nc + 1
+		value = value:sub(#dig + 2)
+	end
+	if nc ~= 4 then return (nc < 4 and PARTIAL) or FAIL, err end
+	return OK
+end
+
+-- ------------------------------------------------------------------------------
+-- ipv4 address with a netmask number on the end
+-- ------------------------------------------------------------------------------
+TYPE["ipv4_nm"] = {}
+TYPE["ipv4_nm"].validator = function(value, mp, kp, tokens, t)
+	local err = "ipv4nm must be standard dotted quad with /netmask"
+	local ipv4, slash, n = value:match("^([%d%.]+)(/?)(%d-)$")
+	if not ipv4 then return FAIL, err end
+	local rc = TYPE["ipv4"].validator(ipv4, mp, kp, tokens, t)
+	if rc == FAIL then return FAIL, err end
+	if rc == PARTIAL then if n=="" and slash=="" then return PARTIAL, err else return FAIL, err end end
+	if slash == "" or n == "" then return PARTIAL, err end
+	if tonumber(n) > 32 then return FAIL, err end
+	return OK
+end
+
+-- ------------------------------------------------------------------------------
+-- ipv4_nm with an optional "default"
+-- ------------------------------------------------------------------------------
+TYPE["ipv4_nm_default"] = {}
+TYPE["ipv4_nm_default"].validator = function(value, mp, kp, tokens, t)
+	local err = "must be ip address with netmask or 'default'"
+	local rc = partial_match(value, {"default"})
+	if rc ~= FAIL then return rc, (rc ~= OK and err) or nil end
+	rc = TYPE["ipv4_nm"].validator(value, mp, kp, tokens, t)
+	return rc, (rc ~= OK and err) or nil
+end
+
+
 
 return {
 	validate = validate,
